728c6bda6ba33c6185231df7d4b7cdb987c0c5ca
Alec Hewitt <ahewitt0000@gmail.com>
2015-01-31 13:46:44 -0500

    added slow button for operator arm translation

--- Projects/FlackCannon/src/org/usfirst/frc/team58/robot/Arm.java
+++ Projects/FlackCannon/src/org/usfirst/frc/team58/robot/Arm.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team58.robot;
 
+import org.usfirst.frc.team58.robot.Joysticks;
+
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -36,14 +38,28 @@ public class Arm {
 	}
 	
 	public static void DoTeleop(){
-		double speed = 0;
-		if (Joysticks.operator.getRawButton(4) && LimitUp.get()){
-			// Go up if Y is pressed and limit switch not triggered
-			speed = 0.5;
-		} else if(Joysticks.operator.getRawButton(3) && LimitDown.get()){ 
-			// Go down if X is pressed and limit switch not triggered
-			speed = -0.5;
-		}
+		//speed of arm translation
+				double speed = 0;
+				//LB is slow button for arm translation
+				boolean slow = Joysticks.driver.getRawButton(5);
+				
+				if (Joysticks.operator.getRawButton(4) && LimitUp.get() && slow){
+					// Go up half-speed if Y is pressed and limit switch not triggered and slow 
+					speed = 0.25;
+				} else if (Joysticks.operator.getRawButton(4) && LimitUp.get()){
+					// Go up normal speed if Y is pressed and limit switch not triggered
+					speed = 0.5;
+				} else if(Joysticks.operator.getRawButton(3) && LimitDown.get() && slow){ 
+					// Go down half-speed if X is pressed and limit switch not triggered and slow
+					speed = -0.25;
+				} else if(Joysticks.operator.getRawButton(3) && LimitDown.get()){
+					// Go down normal speed if X is pressed and limit switch not triggered
+					speed = -0.5;
+				} else {
+					speed = 0;
+				}
+		
+		//set speed of arm
 		SetArm(speed);
 		
 		SetCollector(Joysticks.operator.getZ());
